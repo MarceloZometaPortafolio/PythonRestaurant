b20a11fff7dabd023c5d9d6033de1a68e2db5942
Marcelo Zometa <marcelo.zometa@hotmail.com>
2020-11-16 22:21:20 -0700

    test for user pass

--- src/user.py
+++ src/user.py
@@ -1,11 +1,23 @@
 class User:
     __id = 0
+    __username = ""
+    __password = ""
 
-    def __init__(self, id):
+    def __init__(self, id, *args, **kwargs):
         super().__init__()
         self.__id = id
+        self.__username = kwargs.get('username')
+        self.__password = kwargs.get('password')
 
     def get_id(self):
         return self.__id
 
-    
+    def login(self, username, password):
+        if self.__username == username and self.__password == password:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def register(new_username, new_password):
+        return User(1, username=new_username, password=new_password)
--- tests/test_user.py
+++ tests/test_user.py
@@ -4,7 +4,7 @@ from src.user import User
 class Test_User:
 
     def test_user_can_login_as_admin(self):
-        admin = User(1)
+        admin = User(1, username="username", password="password")
 
         expected = True       
         actual = admin.login("username", "password")
@@ -12,17 +12,14 @@ class Test_User:
         assert expected == actual
 
     def test_user_cant_login_as_admin(self):
-        admin = User(1)
+        admin = User(1, username="username", password="password")
 
         expected = False       
         actual = admin.login("notValid", "password")
 
         assert expected == actual
 
-    def test_user_can_register(self):
-        user = User(1)
+    def test_user_can_register(self):             
+        actual = User.register("username", "password")
 
-        expected = True       
-        actual = user.register("username", "password")
-
-        assert expected == actual
+        assert isinstance(actual, User) == True
